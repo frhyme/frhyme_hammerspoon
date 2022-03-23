247350d6d49738e9938a418f6c04e14bd742c0dd
seunghoon Lee <freerhein@gmail.com>
2022-03-24 00:31:16 +0900

    20220324(Thu): Set mouse control by alt, cmd, hjkl

--- init.lua
+++ init.lua
@@ -10,14 +10,13 @@ IMPLEMENTED
 -------------
 NOT IMPLEMENTED YET
 2022.03.19 - sng_hn.lee - karabiner에서는 기본적으로 FN + 4 -> go launchpad 등이 서포트되는데, hammerspoon 에서는 안됨.
+- 이건 시스템 환경 설정 > 키보드 > 단축키 에서 설정할 수 있어서 수정함.
 2022.03.20 - sng_hn.lee - 키보드로 마우스 움직이고 클릭할 수 있도록 세팅하려면?
-2022.03.21 - sng_hn.lee - Caps Lock key 생성 필요함.
+2022.03.21 - sng_hn.lee - Caps Lock key 생성 필요함. 다만, 이건 아래에 작성한 이유로 잘 안되느 것 같음.
 -------------
 --]]
 ------------------------------------------------------------------------------------
 -- 2022.03.21 - sng_hn.lee - hammerspoon reload by shortcut
-print('-----------------------------------------------------')
-print('-- Hammerspoon console Start')
 function refresh_hammerspoon() 
   hs.hotkey.bind(
     {'option', 'cmd'}, 
@@ -90,9 +89,7 @@ function stroke_arrow(arrow_key)
     local event = require("hs.eventtap").event
     event.newKeyEvent({}, arrow_key, true):post()
     event.newKeyEvent({}, arrow_key, false):post()
-  end
-end
-
+  end end
 function all_arrow_key_binding ()
   local arrow_table = {
     H='left', J='down', K='up', L='right'
@@ -169,8 +166,8 @@ function control_key_change_kor_en()
 
       --control_release_event:start()
       --print("---------------------------------------")
-      --print('containExactly ctrl: ', flags:containExactly({'ctrl'}))
-      --print('keycode: ', keycode)
+      -- print('containExactly ctrl: ', flags:containExactly({'ctrl'}))
+      -- print('keycode: ', keycode)
       if (flags:containExactly({'ctrl'}) == true) then
         -- ctrl pressed : true
         -- ctrl pressed > i pressed true
@@ -191,18 +188,124 @@ function control_key_change_kor_en()
   control_keyevent:start()
 end
 ------------------------------------------------------------------------------------
+-- 2022.03.22 - sng_hn.lee - fn + tab => Caps Lock
+function ctrl_space_to_capslock() 
+  --[[
+  2022.03.23 - sng_hn.lee
+  - HHKB에서는 기본적으로 Fn + tab 에 Caps lock이 mapping되어 있다.
+  - 다만 mac은 keyboard 기본 설정에서 "capslock을 이용해서 입력 소스 전환"을 설정할 수 있다.
+  해당 기능을 활성화할 경우, capslock 키에 한영전환 키가 매핑되고, 키보드에서 capslock을 입력할 수 있는 방법은 사라진다.
+  - 따라서, 해당 기능이 활성화되어 있는 상황에서는 HHKB에서 Fn + tab을 눌러도 capslock이 눌리지 않는다.
+  - 또한, Fn 키는 해피해킹 자체에서 일종의 스위치처럼 키를 변경하는 것이어서, Fn 키가 눌리는 것을 맥 혹은 Hammerspoon에서 인식할 수 없다.
+  - "capslock을 이용해서 입력 소스 전환"을 비활성화하면 capslock을 사용할 수 있지만, 이 경우 맥북 기본 키보드를 사용할 경우 입력 소스 전환이 조금 번거로워짐.
+  - 맥에서의 기본적인 입력 소스 전환은 ctrl + space임. 현재는 ctrl을 사용하여, 한영전환을 하고 있으므로 ctrl + space는 사용하지 않는 상황.
+  - 따라서, ctrl + space에 캡츠락 키가 매핑되도록 함.
+  - ctrl이 눌리므로, 한영 전환도 발생하지 않도록 수정 필요함.
+  --]]
+  -- https://www.hammerspoon.org/docs/hs.hid.html
+  -- 
+  hs.hotkey.bind({"ctrl"}, 'space', 
+    function ()
+      print('ctrl_space')
+      -- 현재는 capslock이 없는 상황이기 때문에, 아래에서 오류가 발생함.
+      -- ** Warning:hs.keycode: key '255' not found in active keymap or ANSI-standard US keyboard layout
+      -- hs.hid.capslock.toggle()
+    end
+  )
+end
+------------------------------------------------------------------------------------
+-- 2022.03.23 - sng_hn.lee
+function print_keycode()
+  key_code_print = hs.eventtap.new (
+    {
+      hs.eventtap.event.types.flagsChanged, 
+      hs.eventtap.event.types.keyDown
+    },
+    function (event)
+      local flags = event:getFlags()
+      local keycode = hs.keycodes.map[event:getKeyCode()]
+      print(keycode)
+    end
+  )
+  
+  key_code_print:start()
+end 
+------------------------------------------------------------------------------------
+-- 2022.03.23 - sng_hn.lee - mouse 움직이도록 설ㅏ
+function move_mouse ()
+  --https://www.hammerspoon.org/docs/hs.mouse.html
+
+  local function func_move_mouse_hjkl (key)
+    local step_x_size = 30.0
+    local step_y_size = 15.0
+
+    local curr_relative_pos = hs.mouse.getRelativePosition()
+    local curr_absolute_pos = hs.mouse.absolutePosition()
+    local curr_abs_x = curr_absolute_pos['x']
+    local curr_abs_y = curr_absolute_pos['y']
+
+    --print('current absolute position: ', curr_abs_x, curr_abs_y)
+
+    if (key == 'H') then
+      --print('go mouse left')
+
+      to_xy_pos = {x=curr_abs_x - step_x_size, y=curr_abs_y}
+      hs.mouse.absolutePosition(to_xy_pos)
+    elseif (key == 'J') then
+      --print('go mouse down')
+
+      to_xy_pos = {x=curr_abs_x, y=curr_abs_y + step_y_size}
+      hs.mouse.absolutePosition(to_xy_pos)
+    elseif (key == 'K') then
+      --print('go mouse up')
+
+      to_xy_pos = {x=curr_abs_x, y=curr_abs_y - step_y_size}
+      hs.mouse.absolutePosition(to_xy_pos)
+    elseif (key == 'L') then
+      --print('go mouse right')
+
+      to_xy_pos = {x=curr_abs_x + step_x_size, y=curr_abs_y}
+      hs.mouse.absolutePosition(to_xy_pos)
+    end
+  end 
+  -- 현재 맥북에서는 abs, rel 값이 동일한 것 같음.
+  for key, value in pairs({'H', 'J', 'K', 'L'}) do
+    hs.hotkey.bind({'cmd', 'alt'}, value, 
+      function () func_move_mouse_hjkl(value) end,
+      function () end,
+      function () func_move_mouse_hjkl(value) end
+    )
+  end
+
+  -- left click
+  hs.hotkey.bind({'cmd', 'alt'}, 'U', 
+    function () 
+      --print('-- click')
+      local curr_absolute_pos = hs.mouse.absolutePosition()
+      hs.eventtap.leftClick(curr_absolute_pos)
+    end, 
+    function () end, 
+    function () end
+  )
+end 
 ------------------------------------------------------------------------------------
 -- MAIN CODE
 function main() 
+  print('-----------------------------------------------------')
+  print('-- Hammerspoon console Start')
+  --print_keycode()
+  ctrl_space_to_capslock()
+
   refresh_hammerspoon()
   all_arrow_key_binding()
   escape_key_en_binding()
   control_key_change_kor_en()
+  move_mouse()
+  -- End of the Code
+  hs.alert.show('Hammerspoon Reloaded Completed!')
+  print('-----------------------------------------------------')
 end
 main()
-------------------------------------------------------------------------------------
--- End of the Code
-hs.alert.show('Hammerspoon Reloaded Completed!')
 ------------------------------------------------------------------------------------
 --[[
 EXAMPLES
